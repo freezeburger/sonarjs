0e6bc8622869c1f8643a5bf3efab9cd6811b01df
Damien Klinnert <mail@damienklinnert.de>
2013-06-02 19:49:31 +0200

    add tests

---
+++ sonarJSTests/sonarJSTests.h
@@ -0,0 +1,13 @@
+//
+//  sonarJSTests.h
+//  sonarJSTests
+//
+//  Created by Damien Klinnert on 02.06.13.
+//  Copyright (c) 2013 Damien Klinnert. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface sonarJSTests : SenTestCase
+
+@end
---
+++ sonarJSTests/sonarJSTests.m
@@ -0,0 +1,32 @@
+//
+//  sonarJSTests.m
+//  sonarJSTests
+//
+//  Created by Damien Klinnert on 02.06.13.
+//  Copyright (c) 2013 Damien Klinnert. All rights reserved.
+//
+
+#import "sonarJSTests.h"
+
+@implementation sonarJSTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in sonarJSTests");
+}
+
+@end
